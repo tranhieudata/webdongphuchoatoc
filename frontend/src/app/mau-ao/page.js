9de78d8e2ebbcb24b50794c7871e8543e7cf220e
tranngochieu <58277561+tranhieudata@users.noreply.github.com>
2026-05-16 07:15:49 +0700

    update

--- frontend/src/app/mau-ao/page.js
+++ frontend/src/app/mau-ao/page.js
@@ -3,7 +3,7 @@
 
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
-import Wrapper from "@/Layout/wrapper";
+
 
 const API = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
 
